6a4a1c74c390e455c8f55dfeee773d17a36f0958
vhqr0 <zq_cmd@163.com>
2024-06-23 13:02:26 +0800

    init: add _bootstrapped flag

--- hy_functional/__init__.py
+++ hy_functional/__init__.py
@@ -1,7 +1,12 @@
 import hy
 
-def _bootstrap():
-    import builtins
-    import hy_functional.core as hfc
-    builtins.__dict__.update(hfc.select_keys(hfc.__dict__, hfc.__all__))
-    builtins._hy_macros.update(hfc.select_keys(hfc._hy_macros, hfc._hy_export_macros))
+_bootstrapped = False
+
+def _bootstrap(force=False):
+    global _bootstrapped
+    if force or not _bootstrapped:
+        import builtins
+        import hy_functional.core as hfc
+        builtins.__dict__.update(hfc.select_keys(hfc.__dict__, hfc.__all__))
+        builtins._hy_macros.update(hfc.select_keys(hfc._hy_macros, hfc._hy_export_macros))
+        _bootstrapped = True
